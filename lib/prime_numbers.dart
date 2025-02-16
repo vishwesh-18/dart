020ced8f2069d10defc927c00574e37269631c39
vishweshkuma <talimkarvishwesh@gmail.com>
2025-02-16 23:37:52 +0530

    15-02-2025

--- lib/prime_numbers.dart
+++ lib/prime_numbers.dart
@@ -30,3 +30,24 @@ void main() {
     }
   }
 }
+
+
+
+// bool isPrime(int num) {
+//   if (num < 2) return false;
+//   for (int i = 2; i < num; i++) {
+//     if (num % i == 0) return false;
+//   }
+//   return true;
+// }
+//
+// void main() {
+//   List primeNumbers = [];
+//   for (int i = 2; i < 100; i++) {
+//     if (isPrime(i)) {
+//       primeNumbers.add(i);
+//     }
+//   }
+//   primeNumbers.forEach((e) => print(e));
+// }
+
